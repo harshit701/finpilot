278e71c3b5223d502a91f65760bf32d0eb3eb438
HarshitDave <daveharshit1@gmail.com>
2026-07-02 18:49:22 +0530

    feat: add Prisma, Swagger and initial user schema

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('FinPilot API')
+    .setDescription('The FinPilot API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
